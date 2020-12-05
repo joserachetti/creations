13f79822c37fd283361821f5c935ad53b7f71514
José Rachetti <joseballe9@gmail.com>
2020-12-04 23:41:26 -0300

    Update Page

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
-var i = 0;
-var txt = 'Developing ;)';
-var speed = 350;
 
-function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("typeText").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-    }
+//Gallery
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
 
-window.onload = typeWriter();
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
